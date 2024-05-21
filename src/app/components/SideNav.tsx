3b823267ceea2834b04bbd7065aa9abc58c98396
Bhanu_Dova <130201202+bhanudova@users.noreply.github.com>
2024-05-21 19:24:48 +0530

    ui changes added

--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -1,17 +1,62 @@
-import Link from 'next/link'
 import React from 'react'
 
 const SideNav = () => {
-  return (
-    <div className='p-4'>
-      <ul>
-        <li><Link href='/dashboard'>Home</Link></li>
-        <li><Link href='/dashboard/customers'>Customer</Link></li>
-        <li><Link href='/dashboard/invoices'>Invoice</Link></li>
-      </ul>
-     
-    </div>
-  )
+    return (
+        <div className='py-1'>
+            <div className='scrollbar' id="style-1">
+                <menu className='px-6 py-3'>
+                    <details>
+                        <summary>Electronics</summary>
+                        <div>Television</div>
+                        <div>Mobiles</div>
+                        <div>Watches</div>
+                    </details>
+                    <details >
+                        <summary>FootWare</summary>
+                        <div>Casuals</div>
+                        <div>Sneakers</div>
+                        <div>Boots</div>
+                    </details>
+                    <details>
+                        <summary>Fashion</summary>
+                        <div>Kids</div>
+                        <div>Men</div>
+                        <div>Women</div>
+                    </details>
+                    <details>
+                        <summary>Electronics</summary>
+                        <div>Television</div>
+                        <div>Mobiles</div>
+                        <div>Watches</div>
+                    </details>
+                    <details >
+                        <summary>FootWare</summary>
+                        <div>Casuals</div>
+                        <div>Sneakers</div>
+                        <div>Boots</div>
+                    </details>
+                    <details>
+                        <summary>Fashion</summary>
+                        <div>Kids</div>
+                        <div>Men</div>
+                        <div>Women</div>
+                    </details>
+                    <details >
+                        <summary>FootWare</summary>
+                        <div>Casuals</div>
+                        <div>Sneakers</div>
+                        <div>Boots</div>
+                    </details>
+                    <details>
+                        <summary>Fashion</summary>
+                        <div>Kids</div>
+                        <div>Men</div>
+                        <div>Women</div>
+                    </details>
+                </menu>
+            </div>
+        </div>
+    )
 }
 
 export default SideNav
